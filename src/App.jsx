92ba065e7535ebef1af8d7d5fbf3ef8b2b7ed25c
Nick Jones <147700520+TheMightyLoafer@users.noreply.github.com>
2024-03-02 17:00:21 +0000

    minor changes to paths to experiment with address bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
@@ -7,7 +7,7 @@ import About from './pages/About'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    
       <Routes>
         <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -15,6 +15,6 @@ export default function App() {
           <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    
   )
 }
