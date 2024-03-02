05fba074e86613437bac33972ffe5d05ad0160d7
Nick Jones <147700520+TheMightyLoafer@users.noreply.github.com>
2024-03-02 16:39:54 +0000

    added router paths to the files in /pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Layout from './components/Layout'
-
-function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<Layout />} />
-    </Routes>
-  
-  </BrowserRouter>
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
