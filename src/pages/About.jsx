9b53f6c101530c76b58719cbd08adb58d7e139bf
TheMightyLoafer <147700520+TheMightyLoafer@users.noreply.github.com>
2024-03-03 14:49:57 -0800

    Adding page content and site content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 export default function About() {
     return (
         <div className='about-page'>
-            <h1 className='about-header'>About Us</h1>
-            <h2>This area is under construction</h2>
-            <p>This is the about page of my website.</p>
+            <h1 className='about-header'>About Me</h1>
+            <h2>Hi, I'm Nick Jones</h2>
+            <h3>Frontend Software Developer</h3>
+            <h4>Techstack: React| JavaScript| HTML| CSS</h4>
+            <p>Hello and welcome to my site!</p>
+            <p></p>
         </div>
     );
 }
---
+++ src/pages/Splash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Splash() {
+    return (
+        <React.Fragment>
+            {/*content for splash page goes here */}
+        </React.Fragment>
+    )
+}
