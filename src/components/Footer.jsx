7fcc8e91115d76ff2fee111ab2747ebdf54c3e74
TheMightyLoafer <147700520+TheMightyLoafer@users.noreply.github.com>
2024-03-03 14:13:11 -0800

    style and format changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return(
   <footer className='footer'>
-    <p className='footer-content'>&copy; {new Date().getFullYear()} | <a href="https://github.com/author">Author</a></p>
+    <p className='footer-content'>&copy; {new Date().getFullYear()} | <a href="https://github.com/TheMightyLoafer">Nicolas Jones</a></p>
   </footer>
   )
     }
