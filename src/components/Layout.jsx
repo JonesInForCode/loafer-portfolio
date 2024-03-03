592c28ca180acafaad4c6552acee25747e49a0ca
TheMightyLoafer <147700520+TheMightyLoafer@users.noreply.github.com>
2024-03-03 12:30:30 -0800

    new styles not applying, bugtesting

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,11 +5,12 @@ import Header from './Header'
 import Footer from './Footer'
 
 export default function Layout() {
+  console.log(styles)
   return (
-    <div className={styles.sitelayout}>
+    <div className={styles.siteWrapper}>
       <Header />
-      <Outlet />
-      <Footer />
+      <Outlet className={styles.contentWindow} />
+      <Footer className={styles.pageFooter}/>
     </div>
   )
 }
