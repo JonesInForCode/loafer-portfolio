2e9f6d2b8ab300a0d5517044d62a26da142412c6
Nick Jones <147700520+TheMightyLoafer@users.noreply.github.com>
2024-03-05 10:35:12 +0000

    starting process of rendering data to browser

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import getData from '../../api'
 
 
 
 export default function Projects() {
+  const [projects, setProjects] = React.useState([]);
+  React.useEffect(() => {
+    getData().then(data => {
+      if (data) {
+        setProjects(data);  
+      }
+    })
+  }, [])
+  console.log(projects)
     return (
-        <h1>This area is under construction...</h1>
+        <div>
+          {projects.map(project => (
+            <div key={project.id}>
+              <h2>{project.title}</h2>
+              <p>{project.description}</p>
+            </div>))}
+        </div>
     )
 }
