fc058c67e9453637e4ca647cbab5fe5a8b33f413
Nick Jones <nico.rjones@outlook.com>
2024-03-07 02:48:58 -0800

    comment out database to prevent unnesesary calls for now

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDbDOEjx--5Wdsrlp7HnaSTV1rYo5TeAlo",
-  authDomain: "loafer-portfolio.firebaseapp.com",
-  projectId: "loafer-portfolio",
-  storageBucket: "loafer-portfolio.appspot.com",
-  messagingSenderId: "89801243994",
-  appId: "1:89801243994:web:a440cd1130f213e5ed8763"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDbDOEjx--5Wdsrlp7HnaSTV1rYo5TeAlo",
+//   authDomain: "loafer-portfolio.firebaseapp.com",
+//   projectId: "loafer-portfolio",
+//   storageBucket: "loafer-portfolio.appspot.com",
+//   messagingSenderId: "89801243994",
+//   appId: "1:89801243994:web:a440cd1130f213e5ed8763"
+// };
 
-export const app = initializeApp(firebaseConfig)
-export const database = getFirestore(app)
+// export const app = initializeApp(firebaseConfig)
+// export const database = getFirestore(app)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,10 @@ export default function Home() {
         <div className='home-page'>
             <h1 className='home-header'>Welcome to my homepage!</h1>
             <p>This area is under construction</p>
+            <br/>
+            <p>This site is to serve as the hub for all my completed projects</p>
+            <p>It was built using React & Firebase</p>
+            <p>Be sure to check back often for new features!</p>
         </div>
     );
 }
