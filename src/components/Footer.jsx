ab08dc2e6b6bdd080ae73c4fefede5b422fdb807
Nick Jones <147700520+TheMightyLoafer@users.noreply.github.com>
2024-03-08 16:49:03 -0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default function Footer() {
+  const now = new Date()
   return(
   <footer className='footer' style={{display: 'flex', justifyContent: 'space-between'}}>
-
+      <p>It is now {now}</p>
       <p className='footer-content'>&copy; {new Date().getFullYear()} <a href="https://github.com/TheMightyLoafer">Nicolas Jones</a></p>
       <p><span>Created using React + Vite + Firebase</span></p>
   </footer>
