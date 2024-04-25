ba007f284944843be643216e2b6768748ab95176
Nick Jones <nico.rjones@outlook.com>
2024-04-25 04:11:01 -0700

    homepage styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,6 +25,23 @@ export default function Home() {
           </section>
         </section>
       </section>
+      <section className={styles.centerSection}>
+        <h1>Welcome!</h1>
+        <p>
+          I'm Nick Jones, Frontend Developer and aspiring Full Stack Engineer. I
+          began learning Software Engineering in October of 2022, exploring
+          multiple disciplines from Java to Game Development, eventually
+          settling into Web Development summer of 2023. I believe in taking a
+          basic foundational approach to my web applications, fully planning out
+          what I want to do and what problems could appear, plotting what needs
+          to be done, and creating a functional framework before even touching
+          the keyboard. This improves both the workflow and keeps things within
+          the scope of what is being done, to avoid project bloat. I continue to
+          build my skills across multiple platforms, already proficient in HTML,
+          CSS, and Javascript, with a big focus on using React as my frontend
+          library.
+        </p>
+      </section>
       <section className={styles.aboutSnip}>
         <AboutSniplet />
       </section>
