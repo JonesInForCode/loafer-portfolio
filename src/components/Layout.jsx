e19970443130f94d2887284f467d3e108c8bfa2f
Nick Jones <nico.rjones@outlook.com>
2024-04-26 04:25:25 -0700

    removed modal warning from the homepage

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,61 +3,37 @@ import { Outlet } from "react-router-dom";
 import styles from "./Layout.module.css";
 import Header from "./Header";
 import Footer from "./Footer";
-import Modal from "./Modal";
 
 export default function Layout() {
-  const [showModal, setShowModal] = React.useState(false);
-  const [showPage, setShowPage] = React.useState(true);
-
   const useWindowSize = () => {
     const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
 
     React.useEffect(() => {
       const handleResize = () => setScreenWidth(window.innerWidth);
+
       window.addEventListener('resize', handleResize);
 
       return () => window.removeEventListener('resize', handleResize);
     }, [])
+
     return screenWidth;
   }
 
   const screenWidth = useWindowSize();
 
-  const isHomePage = window.location.pathname === "/";
-
-  React.useEffect(() => {
-    if (isHomePage) {
-      setShowModal(true);
-      setShowPage(false);
-    }
-  }, []);
-
-  const closeModal = () => {
-    setShowModal(false);
-    setShowPage(true);
-  };
   return (
     <div className={styles.splash}>
-      <Modal isOpen={showModal} onClose={closeModal}>
-        <h2>WARNING!</h2>
-        <p>
-          This website is best viewed on desktops with screen sizes of 720
-          pixels and above. You may run into layout issues if your screen size is below 720 pixels.
-        </p>
-        <p>Your screen size is: {screenWidth} pixels </p>
-      </Modal>
-      {showPage && (<div>
+      <div>
         <div className={styles.siteWrapper}>
           <Header />
           <main className={styles.contentWindow}>
             <Outlet />
           </main>
-
           <div className={styles.pageFooter}>
             <Footer />
           </div>
         </div>
-      </div>)}
+      </div>
     </div>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,33 @@
 import React from "react";
-import styles from "./Pages.module.css"
+import styles from "./Pages.module.css";
 
 export default function Contact() {
   return (
-    <div className={styles.contactCard}>
-      <h1 className={styles.contactHeader}>Contact me</h1>
-      <h2>This area is under construction</h2>
-      <h3>Contact Informaion</h3>
-      <section>
-        <ul>
-          <li>
-            <a href="mailto:themightyloafer.self098@passinbox.com">
-              themightyloafer.self098@passinbox.com
-            </a>
-          </li>
-          <li><a href="https://www.linkedin.com/in/nicolas-jones-0930b6177/">Github</a></li>
-          <li>Dev.to</li>
-        </ul>
-      </section>
-      <p>This is the contact page of my website.</p>
-    </div>
+    <React.Fragment>
+      <div className={styles.contactCard}>
+        <h1 className={styles.contactHeader}>Contact me</h1>
+        <h2>This area is under construction</h2>
+        <h3>Contact Informaion</h3>
+        <section>
+          <ul>
+            <li>
+              <a href="mailto:themightyloafer.self098@passinbox.com">
+                themightyloafer.self098@passinbox.com
+              </a>
+            </li>
+            <li>
+              <a href="https://www.github.com/jonesinforcode">Github</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/nicolas-jones-0930b6177/">
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </section>
+        <p>This is the contact page of my website.</p>
+      </div>
+      <div className={styles.contactForm}></div>
+    </React.Fragment>
   );
 }
