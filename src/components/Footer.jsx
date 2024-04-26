86781ad0ea4f0eb04eccfed90a32f6747675da0a
Nick Jones <nico.rjones@outlook.com>
2024-04-26 04:50:07 -0700

    fixed broken link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return(
   <footer className={styles.pageFooter} style={{display: 'flex', justifyContent: 'space-between'}}>
 
-      <p className={styles.pageFooterContent}>&copy; {new Date().getFullYear()} <a href="https://github.com/TheMightyLoafer">Nicolas Jones</a></p>
+      <p className={styles.pageFooterContent}>&copy; {new Date().getFullYear()} <a href="https://github.com/JonesInForCode">Nicolas Jones</a></p>
       <p className={styles.pageFooterContent}><span>Created using React + Vite + Firebase</span></p>
   </footer>
   )
